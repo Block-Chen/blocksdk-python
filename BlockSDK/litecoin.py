8416d5dd9b8aff69e5d6449ab40d25c727c11762
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 12:03:33 +0900

    Update litecoin.py

--- BlockSDK/litecoin.py
+++ BlockSDK/litecoin.py
@@ -1,23 +1,23 @@
 from BlockSDK.base import Base
 class Litecoin(Base):
 	def getBlockChain(self,request = {}):
-			return self.request("GET","/ltc/block")
-		
+		return self.request("GET","/ltc/info")
+	
 	def getBlock(self,request = {}):
-		
 		if not('rawtx' in request) or not request['rawtx']:
 			request['rawtx'] = False
+			
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/ltc/block/" + str(request['block']) ,{
+		return self.request("GET","/ltc/blocks/" + str(request['block']),{
 			"rawtx" : request['rawtx'],
 			"offset" : request['offset'],
 			"limit" : request['limit']
 		})
-
+	
 	
 	def getMemPool(self,request = {}):
 		if not('rawtx' in request) or not request['rawtx']:
@@ -46,7 +46,7 @@ class Litecoin(Base):
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/ltc/address/" + str(request['address']) + "",{
+		return self.request("GET","/ltc/addresses/{request['address']}",{
 			"reverse" : request['reverse'],
 			"rawtx" : request['rawtx'],
 			"offset" : request['offset'],
@@ -55,43 +55,39 @@ class Litecoin(Base):
 
 	
 	def getAddressBalance(self,request = {}):
-		return self.request("GET","/ltc/address/" + str(request['address']) + "/balance")
+		return self.request("GET","/ltc/addresses/{request['address']}/balance")
 	 
-	def listWallet(self,request = {}):
+	def getWallets(self,request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/ltc/wallet",{
+		return self.request("GET","/ltc/wallets",{
 			"offset" : request['offset'],
 			"limit" : request['limit']
 		})
 
 	
-	def createWallet(self,request = {}):
+	def createHdWallet(self,request = {}):
 		if not('name' in request) or not request['name']:
 			request['name'] = None
 		
-		return self.request("POST","/ltc/wallet",{
+		return self.request("POST","/ltc/wallet/hd",{
 			"name" : request['name']
 		})
-
 	
 	def loadWallet(self,request = {}):
 
-		return self.request("POST","/ltc/wallet/" + str(request['wallet_id']) + "/load",{
-			"seed_wif" : request['seed_wif'],
+		return self.request("POST","/ltc/wallets/" + str(request['wallet_id']) + "/load",{
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 
+	def unloadWallet(self,request = {}):
+		return self.request("POST","/ltc/wallets/" + str(request['wallet_id']) + "/unload")
 	
-	def unLoadWallet(self,request = {}):
-		
-		return self.request("POST","/ltc/wallet/" + str(request['wallet_id']) + "/unload")
-
-	
-	def listWalletAddress(self,request = {}):
+	def getWalletAddress(self,request = {}):
 		if not('address' in request) or not request['address']:
 			request['address'] = None
 		if not('hdkeypath' in request) or not request['hdkeypath']:
@@ -102,97 +98,80 @@ class Litecoin(Base):
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/ltc/wallet/" + str(request['wallet_id']) + "/address",{
+		return self.request("GET","/ltc/wallets/" + str(request['wallet_id']) + "/addresses",{
 			"address" : request['address'],
 			"hdkeypath" : request['hdkeypath'],
 			"offset" : request['offset'],
 			"limit" : request['limit']
 		})
-
 	
 	def createWalletAddress(self,request = {}):
-
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		if not('wif' in request) or not request['wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/ltc/wallet/" + str(request['wallet_id']) + "/address",{
-			"seed_wif" : request['seed_wif'],
+		return self.request("POST","/ltc/wallets/" + str(request['wallet_id']) + "/addresses",{
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 	
+	def getWalletBalance(self,request = {}):	
+		return self.request("GET","/ltc/wallets/" + str(request['wallet_id']) + "/balance")		
 	
-	def getWalletBalance(self,request = {}):
-		
-		return self.request("GET","/ltc/wallet/" + str(request['wallet_id']) + "/balance")
-
-			
-	
-	def getWalletTransaction(self,request = {}):
-		if not('category' in request) or not request['category']:
-			request['category'] = 'all'
+	def getWalletTransactions(self,request = {}):
 		if not('order' in request) or not request['order']:
 			request['order'] = 'desc'
-	
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
-		
-		return self.request("GET","/ltc/wallet/" + str(request['wallet_id']) + "/transaction",{
-			"category" : request['category'],
+		if not('type' in request) or not request['type']:
+			request['type'] = 'all'
+
+		return self.request("GET","/ltc/wallets/" + str(request['wallet_id']) + "/transaction",{
+			"type" : request['type'],
 			"order" : request['order'],
 			"offset" : request['offset'],
 			"limit" : request['limit']
 		})
-
 		
 	def sendToAddress(self,request = {}):
 		if(not('kbfee' in request) or not request['kbfee']):
 			blockChain = self.getBlockChain()
 			request['kbfee'] = blockChain['medium_fee_per_kb']
 
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		
+		if not('wif' in request) or not request['wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/ltc/wallet/" + str(request['wallet_id']) + "/sendtoaddress",{
+		return self.request("POST","/ltc/wallets/" + str(request['wallet_id']) + "/sendtoaddress",{
 			"address" : request['address'],
 			"amount" : request['amount'],
-			"seed_wif" : request['seed_wif'],
+			"wif" : request['wif'],
 			"password" : request['password'],
 			"kbfee" : request['kbfee']
 		})
-
 	
 	def sendMany(self,request = {}):
-
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		
+		if not('wif' in request) or not request['wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/ltc/wallet/" + str(request['wallet_id']) + "/sendmany",{
+		return self.request("POST","/ltc/wallets/" + str(request['wallet_id']) + "/sendmany",{
 			"to" : request['to'],
-			"seed_wif" : request['seed_wif'],
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 
-	
 	def sendTransaction(self,request = {}):
-		return self.request("POST","/ltc/transaction",{
-			"sign_hex" : request['sign_hex']
+		return self.request("POST","/ltc/transactions/send",{
+			"hex" : request['hex']
 		})
 	
-	
-	def getTransaction(self,request = {}):	
-		return self.request("GET","/ltc/transaction/" + str(request['hash']) + "")
-# litecoin = Litecoin('ad38QFTevz8fEEAG4fKsf4T5L8pwqgcy6LXMHpqU')
-# print(litecoin.getBlock({
-# 	"block" : 1520000,
-#     "rawtx" :True,
-#     "offset" :0,
-#     "limit" :10
-# }))
+	def getTransaction(self,request = {}):
+		return self.request("GET","/ltc/transactions/" + str(request['hash']) + "")
