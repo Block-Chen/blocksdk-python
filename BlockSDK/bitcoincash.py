ddec552e5a00871ec38ff156b11cf0270ebadb61
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 11:56:31 +0900

    Update bitcoincash.py

--- BlockSDK/bitcoincash.py
+++ BlockSDK/bitcoincash.py
@@ -1,7 +1,7 @@
 from BlockSDK.base import Base
 class BitcoinCash(Base):
 	def getBlockChain(self,request = {}):		
-		return self.request("GET","/bch/block")
+		return self.request("GET","/bch/info")
 	
 	def getBlock(self,request = {}):
 		if not('rawtx' in request) or not request['rawtx']:
@@ -11,7 +11,7 @@ class BitcoinCash(Base):
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/bch/block/" + str(request['block']) + "",{
+		return self.request("GET","/bch/blocks/" + str(request['block']) + "",{
 			"rawtx" : request['rawtx'],
 			"offset" : request['offset'],
 			"limit" : request['limit']
@@ -44,7 +44,7 @@ class BitcoinCash(Base):
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/bch/address/" + str(request['address']) + "",{
+		return self.request("GET","/bch/addresses/" + str(request['address']) + "",{
 			"reverse" : request['reverse'],
 			"rawtx" : request['rawtx'],
 			"offset" : request['offset'],
@@ -53,42 +53,42 @@ class BitcoinCash(Base):
 
 	
 	def getAddressBalance(self,request = {}):
-		return self.request("GET","/bch/address/" + str(request['address']) + "/balance")
+		return self.request("GET","/bch/addresses/" + str(request['address']) + "/balance")
 	 
-	def listWallet(self,request = {}):
+	def getWallet(self,request = {}):
 
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/bch/wallet",{
+		return self.request("GET","/bch/wallets",{
 			"offset" : request['offset'],
 			"limit" : request['limit']
 		})
 
 	
-	def createWallet(self,request = {}):
+	def createHdWallet(self,request = {}):
 		if not('name' in request) or not request['name']:
 			request['name'] = None
 		
-		return self.request("POST","/bch/wallet",{
+		return self.request("POST","/bch/wallets/hd",{
 			"name" : request['name']
 		})
 
 	
 	def loadWallet(self,request = {}):
-		return self.request("POST","/bch/wallet/" + str(request['wallet_id']) + "/load",{
-			"seed_wif" : request['seed_wif'],
+		return self.request("POST","/bch/wallets/" + str(request['wallet_id']) + "/load",{
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 
 	
-	def unLoadWallet(self,request = {}):
-		return self.request("POST","/bch/wallet/" + str(request['wallet_id']) + "/unload")
+	def unloadWallet(self,request = {}):
+		return self.request("POST","/bch/wallets/" + str(request['wallet_id']) + "/unload")
 
 	
-	def listWalletAddress(self,request = {}):
+	def getWalletAddress(self,request = {}):
 
 		if not('address' in request) or not request['address']:
 			request['address'] = None
@@ -99,7 +99,7 @@ class BitcoinCash(Base):
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/bch/wallet/" + str(request['wallet_id']) + "/address",{
+		return self.request("GET","/bch/wallets/" + str(request['wallet_id']) + "/addresses",{
 			"address" : request['address'],
 			"hdkeypath" : request['hdkeypath'],
 			"offset" : request['offset'],
@@ -108,33 +108,33 @@ class BitcoinCash(Base):
 
 	
 	def createWalletAddress(self,request = {}):
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		if not('wif' in request) or not request['seed_wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/bch/wallet/" + str(request['wallet_id']) + "/address",{
-			"seed_wif" : request['seed_wif'],
+		return self.request("POST","/bch/wallets/" + str(request['wallet_id']) + "/addresses",{
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 		
 	
 	def getWalletBalance(self,request = {}):
-		return self.request("GET","/bch/wallet/" + str(request['wallet_id']) + "/balance")
+		return self.request("GET","/bch/wallets/" + str(request['wallet_id']) + "/balance")
 
-	def getWalletTransaction(self,request = {}):
+	def getWalletTransactions(self,request = {}):
 
 		if not('order' in request) or not request['order']:
 			request['order'] = 'desc'
-		if not('category' in request) or not request['category']:
-			request['category'] = 'all'
+		if not('type' in request) or not request['type']:
+			request['type'] = 'all'
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/bch/wallet/" + str(request['wallet_id']) + "/transaction",{
-			"category" : request['category'],
+		return self.request("GET","/bch/wallets/" + str(request['wallet_id']) + "/transaction",{
+			"type" : request['type'],
 			"order" : request['order'],
 			"offset" : request['offset'],
 			"limit" : request['limit']
@@ -146,15 +146,15 @@ class BitcoinCash(Base):
 			blockChain = self.getBlockChain()
 			request['kbfee'] = blockChain['medium_fee_per_kb']
 
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		if not('wif' in request) or not request['wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/bch/wallet/" + str(request['wallet_id']) + "/sendtoaddress",{
+		return self.request("POST","/bch/wallets/" + str(request['wallet_id']) + "/sendtoaddress",{
 			"address" : request['address'],
 			"amount" : request['amount'],
-			"seed_wif" : request['seed_wif'],
+			"wif" : request['wif'],
 			"password" : request['password'],
 			"kbfee" : request['kbfee']
 		})
@@ -162,18 +162,22 @@ class BitcoinCash(Base):
 	
 	def sendMany(self,request = {}):
 		
-		if not('seed_wif' in request) or not request['seed_wif']:
-			request['seed_wif'] = None
+		if not('wif' in request) or not request['wif']:
+			request['wif'] = None
 		if not('password' in request) or not request['password']:
 			request['password'] = None
 		
-		return self.request("POST","/bch/wallet/" + str(request['wallet_id']) + "/sendmany",{
+		return self.request("POST","/bch/wallets/" + str(request['wallet_id']) + "/sendmany",{
 			"to" : request['to'],
-			"seed_wif" : request['seed_wif'],
+			"wif" : request['wif'],
 			"password" : request['password']
 		})
 	
+	def sendTransaction(self,request = {}):
+		return self.request("POST","/bch/transactions/send",{
+			"hex" : request['hex']
+		})
+	
 	def getTransaction(self,request = {}):
-		return self.request("GET","/bch/transaction/" + str(request['hash']) + "")
-
-
+		return self.request("GET","/bch/transactions/" + str(request['hash']) + "")
+	
