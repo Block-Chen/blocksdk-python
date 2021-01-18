a6c2e6ceb420425bc4cd6e78cd2f538519f8cb7f
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 12:49:30 +0900

    Update blocksdk.py

--- BlockSDK/blocksdk.py
+++ BlockSDK/blocksdk.py
@@ -6,7 +6,9 @@ from BlockSDK.bitcoincash import BitcoinCash
 from BlockSDK.monero import Monero
 from BlockSDK.webhook import WebHook
 from BlockSDK.litecoin import Litecoin
-from BlockSDK.price import Price
+from BlockSDK.market import Market
+from BlockSDK.token import Token
+from BlockSDK.tool import Tool
 
 class BlockSDK(Base):
 	def __init__(self, api_token):
@@ -24,9 +26,6 @@ class BlockSDK(Base):
 	def createMonero(self):
 		return Monero(self.api_token)
 
-	def createPrice(self):
-		return Price(self.api_token)
-
 	def createWebHook(self):
 		return WebHook(self.api_token)
 
@@ -35,3 +34,12 @@ class BlockSDK(Base):
 
 	def createBitcoinCash(self):
 		return BitcoinCash(self.api_token)
+	
+	def createMarket(self):
+		return Market(self.api_token)
+	
+	def createToken(self):
+		return Token(self.api_token)
+	
+	def createTool(self):
+		return Tool(self.api_token)
