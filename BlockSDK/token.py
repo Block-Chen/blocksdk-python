07f5aa5d35c9e9c99f9b9097f2e1714640fea22b
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 12:45:44 +0900

    Update token.py

--- BlockSDK/token.py
+++ BlockSDK/token.py
@@ -1,4 +1,4 @@
 from BlockSDK.base import Base
 class Token(Base):
 	def getUsages(self, request = {}):
-		return self.request("POST","/token/usage",{"stat_date" : request['stat_date'],"end_date" : request['end_date']})
+		return self.request("GET","/token/usage",{"stat_date" : request['stat_date'],"end_date" : request['end_date']})
