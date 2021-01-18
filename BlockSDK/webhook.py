28481c974e01bba6fea35458a9ad861da4982050
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-01-18 12:53:09 +0900

    Update webhook.py

--- BlockSDK/webhook.py
+++ BlockSDK/webhook.py
@@ -1,39 +1,39 @@
 from BlockSDK.base import Base
 class WebHook(Base):
 	def create(self,request={}):
-		return self.request("POST","/hook",{"callback" : request['callback'],"category" : request['category'],"address" : request['address']})
+		return self.request("POST","/hooks",{"callback_url" : request['callback_url'],"symbol" : request['symbol'],"address" : request['address']})
 	
 	def list(self,request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10			
-		return self.request("GET","/hook",{"offset" : request['offset'],"limit" : request['limit']})
+		return self.request("GET","/hooks",{"offset" : request['offset'],"limit" : request['limit']})
 		
 		
 	def get(self,request={}):
-		return self.request("GET","/hook/" + str(request['hook_id']))
+		return self.request("GET","/hooks/" + str(request['hook_id']))
 
 	def delete(self,request={}):
-		return self.request("POST","/hook/" + str(request['hook_id']) + "/delete")
+		return self.request("DELETE","/hooks/" + str(request['hook_id']))
 	
 	
-	def listResponse(self,request = {}):
+	def getResponses(self,request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
-		return self.request("GET","/hook/response",{"offset" : request['offset'],"limit" : request['limit']})			
+		return self.request("GET","/hooks/responses",{"offset" : request['offset'],"limit" : request['limit']})			
 			
 	
-	def getResponse(self,request={}):
+	def getHookResponses(self,request={}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 	
-		return self.request("GET","/hook/" + str(request['hook_id']) + "/response",{"offset" : request['offset'],"limit" : request['limit']})			
+		return self.request("GET","/hooks/" + str(request['hook_id']) + "/responses",{"offset" : request['offset'],"limit" : request['limit']})			
 
 
 # wb = WebHook('B1zZARyW1d2FdqWxPUpB79izHmtAc2Az693WF9DD')
