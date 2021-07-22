7152317853a1054404e376676c14d7cb106a1280
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-07-22 17:37:24 +0900

    Update market.py

--- BlockSDK/market.py
+++ BlockSDK/market.py
@@ -2,6 +2,24 @@ from BlockSDK.base import Base
 class Market(Base):
 	def getExchanges(self, request = {}):
 		return self.request("GET","/market/exchanges")		
+
+	def getAssets(self, request = {}):
+		return self.request("GET","/market/assets")		
+
+	def getAsset(self, request = {}):
+		return self.request("GET","/market/assets/" + request['asset_id'])		
+
+	def getOhlcvLast(self, request = {}):
+		return self.request("GET","/market/ohlcv/latest",request)		
+
+	def getOhlcvHistory(self, request = {}):
+		return self.request("GET","/market/ohlcv/history/" + request['symbol'],request)		
+	
+	def getExchangeOhlcvLast(self, request = {}):
+		return self.request("GET","/market/ohlcv/" + request['exchage_id'] + "/latest",request)		
+	
+	def getExchangeOhlcvHistory(self, request = {}):
+		return self.request("GET","/market/ohlcv/" + request['exchage_id'] + "/history/" + request['symbol'],request)		
 	
 	def getTrades(self, request = {}):
 		if not('from' in request) or not request['from']:
@@ -9,22 +27,34 @@ class Market(Base):
 		if not('to' in request) or not request['to']:
 			request['to'] = "USD"
        
-		return self.request("GET","/market/trades/" + str(request['block']),{"from" : request['from'],"to": request['to']})
+		return self.request("GET","/market/trades/",{
+            "from" : request['from'],
+            "to": request['to']
+        })
     
-  def getRates(self, request = {}):
+    def getRates(self, request = {}):
        
-		return self.request("GET","/market/rates/" + str(request['from']),{"to" : request['to'],"from_amount": request['from_amount']})
+		return self.request("GET","/market/rates/" + request['from'],{
+            "to" : request['to'],
+            "from_amount": request['from_amount']
+        })
     
-  def getExchangeTrades(self, request = {}):
+    def getExchangeTrades(self, request = {}):
 		if not('from' in request) or not request['from']:
 			request['from'] = None
 		if not('to' in request) or not request['to']:
 			request['to'] = "USD"
        
-		return self.request("GET","/market/trades/" + str(request['exchage_id']),{"from" : request['from'],"to": request['to']})
+		return self.request("GET","/market/trades/" + request['exchage_id'],{
+            "from" : request['from'],
+            "to": request['to']
+        })
     
-  def getExchangeRates(self, request = {}):
+    def getExchangeRates(self, request = {}):
 		if not('to' in request) or not request['to']:
 			request['to'] = "USD"
        
-		return self.request("GET","/market/rates/" + str(request['exchage_id']) + "/" + str(request['from']),{"to" : request['to'],"from_amount": request['from_amount']})
+		return self.request("GET","/market/rates/" + str(request['exchage_id']) + "/" + str(request['from']),{
+            "to" : request['to'],
+            "from_amount": request['from_amount']
+        })
