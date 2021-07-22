67b20c6bedfeece48044f295ed2e0304aed54e76
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-07-22 18:03:21 +0900

    Update binancesmart.py

--- BlockSDK/binancesmart.py
+++ BlockSDK/binancesmart.py
@@ -76,13 +76,13 @@ class BinanceSmart(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/bsc/addresses/" + request['from'] + "/sendtoaddress",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit']
+		})
 	
 	
 	def sendTransaction(self, request = {}):
@@ -109,15 +109,15 @@ class BinanceSmart(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/bsc/erc20-tokens/" + request['contract_address'] + "/" + request['from'] + "/transfer",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit']
+		})
 
-    def getNfts(self, request = {}):
+    	def getNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -128,7 +128,7 @@ class BinanceSmart(Base):
 			"limit" : request['limit']
 		})
         
-    def getOwnerNfts(self, request = {}):
+    	def getOwnerNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -139,7 +139,7 @@ class BinanceSmart(Base):
 			"limit" : request['limit']
 		})
         
-    def getCreatorNfts(self, request = {}):
+    	def getCreatorNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -150,51 +150,51 @@ class BinanceSmart(Base):
 			"limit" : request['limit']
 		})
         
-    def getAuctionNfts(self, request = {}):
+    	def getAuctionNfts(self, request = {}):
 		if not('order_by' in request) or not request['order_by']:
 			request['order_by'] = 'end_time'
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+       		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/bsc/bep721-tokens/" + request['contract_address'] + "/auction",{
 			"order_by" : request['order_by'],
-            "order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+            		"order_direction" : request['order_direction'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getSaleNfts(self, request = {}):
+    	def getSaleNfts(self, request = {}):
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/bsc/bep721-tokens/" + request['contract_address'] + "/" + request['seller_address'] + "/sale",{
 			"order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getNftBids(self, request = {}):
-        if not('rawtx' in request) or not request['rawtx']:
+   	 def getNftBids(self, request = {}):
+        	if not('rawtx' in request) or not request['rawtx']:
 			request['rawtx'] = 0
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+       		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/bsc/bep721-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/bid",{
 			"order_direction" : request['order_direction'],
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
+            		"rawtx" : request['rawtx'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
@@ -210,10 +210,10 @@ class BinanceSmart(Base):
 			request['limit'] = 10
 		
 		return self.request("GET","/bsc/bep721-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/transfers",{
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
-            "limit" : request['limit']
-        })
+		    "rawtx" : request['rawtx'],
+		    "offset" : request['offset'],
+		    "limit" : request['limit']
+		})
         
 	def getContractRead(self, request = {}):
 		if not('parameter_type' in request) or not request['parameter_type']:
@@ -223,11 +223,11 @@ class BinanceSmart(Base):
 
 		
 		return self.request("POST","/bsc/contracts/" + request['contract_address'] + "/read",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data']
+		})
 
 	def getContractWrite(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -244,16 +244,16 @@ class BinanceSmart(Base):
 
 		
 		return self.request("POST","/bsc/contracts/" + request['contract_address'] + "/write",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getContractWriteFees(self, request = {}):
 		if not('gas_limit' in request) or not request['gas_limit']:
@@ -266,11 +266,11 @@ class BinanceSmart(Base):
 
 		
 		return self.request("POST","/bsc/contracts/" + request['contract_address'] + "/write/fees",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
