dbd75caed90d6b1bb0643cd9aad0c5477ccbd689
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-07-22 18:06:39 +0900

    Update klaytn.py

--- BlockSDK/klaytn.py
+++ BlockSDK/klaytn.py
@@ -67,7 +67,7 @@ class Klaytn(Base):
 	def sendToAddress(self, request = {}):
 		if not('nonce' in request) or not request['nonce']:
 			request['nonce'] = None
-        if not('data' in request) or not request['data']:
+        	if not('data' in request) or not request['data']:
 			request['data'] = None
 		if not('private_key' in request) or not request['private_key']:
 			request['private_key'] = None
@@ -77,18 +77,18 @@ class Klaytn(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/klay/addresses/" + request['from'] + "/sendtoaddress",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gas_limit" : request['gas_limit']
+		})
 	
 	
 	def sendTransaction(self, request = {}):
 		return self.request("POST","/klay/transactions/send",{
-            "hex" : request['hex']
-        })
+		    "hex" : request['hex']
+		})
 	
 	def getTransaction(self, request = {}):
 		return self.request("GET","/klay/transactions/" + request['hash'])
@@ -108,12 +108,12 @@ class Klaytn(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/klay/kip7-tokens/" + request['contract_address'] + "/" + request['from'] + "/transfer",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getKIP7Sign(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -124,12 +124,12 @@ class Klaytn(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/klay/kip7-tokens/" + request['contract_address'] + "/" + request['from'] + "/sign",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getKIP7Feedelegated(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -140,20 +140,20 @@ class Klaytn(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/klay/kip7-tokens/" + request['contract_address'] + "/" + request['fee_payer'] + "/transfer/feedelegated",{
-            "from" : request['from'],
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit'],
-            "nonce" : request['nonce'],
-            "v" : request['v'],
-            "r" : request['r'],
-            "s" : request['s']
-        })
+		    "from" : request['from'],
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit'],
+		    "nonce" : request['nonce'],
+		    "v" : request['v'],
+		    "r" : request['r'],
+		    "s" : request['s']
+		})
 
-    def getNfts(self, request = {}):
+    	def getNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -164,7 +164,7 @@ class Klaytn(Base):
 			"limit" : request['limit']
 		})
         
-    def getOwnerNfts(self, request = {}):
+    	def getOwnerNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -175,7 +175,7 @@ class Klaytn(Base):
 			"limit" : request['limit']
 		})
         
-    def getCreatorNfts(self, request = {}):
+    	def getCreatorNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -186,51 +186,51 @@ class Klaytn(Base):
 			"limit" : request['limit']
 		})
         
-    def getAuctionNfts(self, request = {}):
+    	def getAuctionNfts(self, request = {}):
 		if not('order_by' in request) or not request['order_by']:
 			request['order_by'] = 'end_time'
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/klay/kip17-tokens/" + request['contract_address'] + "/auction",{
 			"order_by" : request['order_by'],
-            "order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+            		"order_direction" : request['order_direction'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getSaleNfts(self, request = {}):
+    	def getSaleNfts(self, request = {}):
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/klay/kip17-tokens/" + request['contract_address'] + "/" + request['seller_address'] + "/sale",{
 			"order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getNftBids(self, request = {}):
-        if not('rawtx' in request) or not request['rawtx']:
+    	def getNftBids(self, request = {}):
+        	if not('rawtx' in request) or not request['rawtx']:
 			request['rawtx'] = 0
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/klay/kip17-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/bid",{
 			"order_direction" : request['order_direction'],
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
+		    	"rawtx" : request['rawtx'],
+		    	"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
@@ -246,10 +246,10 @@ class Klaytn(Base):
 			request['limit'] = 10
 		
 		return self.request("GET","/klay/kip17-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/transfers",{
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
-            "limit" : request['limit']
-        })
+		    "rawtx" : request['rawtx'],
+		    "offset" : request['offset'],
+		    "limit" : request['limit']
+		})
         
 	def getContractRead(self, request = {}):
 		if not('parameter_type' in request) or not request['parameter_type']:
@@ -259,11 +259,11 @@ class Klaytn(Base):
 
 		
 		return self.request("POST","/klay/contracts/" + request['contract_address'] + "/read",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data']
+		})
 
 	def getContractWrite(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -280,16 +280,16 @@ class Klaytn(Base):
 
 		
 		return self.request("POST","/klay/contracts/" + request['contract_address'] + "/write",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getContractWriteSign(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -306,16 +306,16 @@ class Klaytn(Base):
 
 		
 		return self.request("POST","/klay/contracts/" + request['contract_address'] + "/write/sign",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getContractWriteFeedelegated(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -332,18 +332,18 @@ class Klaytn(Base):
 
 		
 		return self.request("POST","/klay/contracts/" + request['contract_address'] + "/write/feedelegated",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "fee_payer" : request['fee_payer'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "amount" : request['amount'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "fee_payer" : request['fee_payer'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "amount" : request['amount'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getContractWriteFees(self, request = {}):
 		if not('gas_limit' in request) or not request['gas_limit']:
@@ -356,11 +356,11 @@ class Klaytn(Base):
 
 		
 		return self.request("POST","/klay/contracts/" + request['contract_address'] + "/write/fees",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
