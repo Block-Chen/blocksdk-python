7f8fe14742e81ba5b778471f6c3c54beacab27a7
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-07-22 17:53:31 +0900

    Update ethereum.py

--- BlockSDK/ethereum.py
+++ BlockSDK/ethereum.py
@@ -12,10 +12,10 @@ class Ethereum(Base):
 			request['limit'] = 10
        
 		return self.request("GET","/eth/blocks/" + str(request['block']),{
-            "rawtx" : request['rawtx'],
-            "offset": request['offset'],
-            "limit" : request['limit']
-        })
+		    "rawtx" : request['rawtx'],
+		    "offset": request['offset'],
+		    "limit" : request['limit']
+		})
 	
 	def getMemPool(self, request = {}):
 		if not('rawtx' in request) or not request['rawtx']:
@@ -26,10 +26,10 @@ class Ethereum(Base):
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/mempool",{
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
-            "limit" : request['limit']
-        })
+		    "rawtx" : request['rawtx'],
+		    "offset" : request['offset'],
+		    "limit" : request['limit']
+		})
 	
 	
 	def getAddresses(self, request = {}):
@@ -45,9 +45,9 @@ class Ethereum(Base):
 	
 	def loadAddress(self, request = {}):
 		return self.request("POST","/eth/addresses/" + request['address'] + "/load",{
-            "private_key" : request['private_key'],
-            "password" : request['password']
-        })
+		    "private_key" : request['private_key'],
+		    "password" : request['password']
+		})
 	
 	def unloadAddress(self, request = {}):
 		return self.request("POST","/eth/addresses/" + request['address'] + "/unload")
@@ -70,11 +70,11 @@ class Ethereum(Base):
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/addresses/" + request['address'],{
-            "reverse" : request['reverse'],
-            "rawtx"  : request['rawtx'],
-            "offset" : request['offset'],
-            "limit"  : request['limit']
-        })
+		    "reverse" : request['reverse'],
+		    "rawtx"  : request['rawtx'],
+		    "offset" : request['offset'],
+		    "limit"  : request['limit']
+		})
 	
 	
 	def getAddressBalance(self, request = {}):
@@ -92,19 +92,19 @@ class Ethereum(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/eth/addresses/" + request['from'] + "/sendtoaddress",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit']
+		})
 	
 	
 	def sendTransaction(self, request = {}):
 		return self.request("POST","/eth/transactions/send",{
-            "hex" : request['hex']
-        })
+		    "hex" : request['hex']
+		})
 	
 	def getTransaction(self, request = {}):
 		return self.request("GET","/eth/transactions/" + request['hash'])
@@ -127,15 +127,15 @@ class Ethereum(Base):
 			request['gas_limit'] = None
 		
 		return self.request("POST","/eth/erc20-tokens/" + request['contract_address'] + "/" + request['from'] + "/transfer",{
-            "to" : request['to'],
-            "amount" : request['amount'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "gwei" : request['gwei'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "to" : request['to'],
+		    "amount" : request['amount'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "gwei" : request['gwei'],
+		    "gas_limit" : request['gas_limit']
+		})
 
-    def getNfts(self, request = {}):
+	def getNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -146,7 +146,7 @@ class Ethereum(Base):
 			"limit" : request['limit']
 		})
         
-    def getOwnerNfts(self, request = {}):
+	def getOwnerNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -157,7 +157,7 @@ class Ethereum(Base):
 			"limit" : request['limit']
 		})
         
-    def getCreatorNfts(self, request = {}):
+	def getCreatorNfts(self, request = {}):
 		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
@@ -168,51 +168,51 @@ class Ethereum(Base):
 			"limit" : request['limit']
 		})
         
-    def getAuctionNfts(self, request = {}):
+	def getAuctionNfts(self, request = {}):
 		if not('order_by' in request) or not request['order_by']:
 			request['order_by'] = 'end_time'
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/erc721-tokens/" + request['contract_address'] + "/auction",{
 			"order_by" : request['order_by'],
-            "order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+		    	"order_direction" : request['order_direction'],
+		    	"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getSaleNfts(self, request = {}):
+	def getSaleNfts(self, request = {}):
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+        	if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/erc721-tokens/" + request['contract_address'] + "/" + request['seller_address'] + "/sale",{
 			"order_direction" : request['order_direction'],
-            "offset" : request['offset'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
-    def getNftBids(self, request = {}):
-        if not('rawtx' in request) or not request['rawtx']:
+    	def getNftBids(self, request = {}):
+        	if not('rawtx' in request) or not request['rawtx']:
 			request['rawtx'] = 0
 		if not('order_direction' in request) or not request['order_direction']:
 			request['order_direction'] = 'desc'
-        if not('offset' in request) or not request['offset']:
+		if not('offset' in request) or not request['offset']:
 			request['offset'] = 0
 		if not('limit' in request) or not request['limit']:
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/erc721-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/bid",{
 			"order_direction" : request['order_direction'],
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
+            		"rawtx" : request['rawtx'],
+            		"offset" : request['offset'],
 			"limit" : request['limit']
 		})
         
@@ -228,10 +228,10 @@ class Ethereum(Base):
 			request['limit'] = 10
 		
 		return self.request("GET","/eth/erc721-tokens/" + request['contract_address'] + "/" + request['token_id'] + "/transfers",{
-            "rawtx" : request['rawtx'],
-            "offset" : request['offset'],
-            "limit" : request['limit']
-        })
+		    "rawtx" : request['rawtx'],
+		    "offset" : request['offset'],
+		    "limit" : request['limit']
+		})
         
 	def getContractRead(self, request = {}):
 		if not('parameter_type' in request) or not request['parameter_type']:
@@ -241,11 +241,11 @@ class Ethereum(Base):
 
 		
 		return self.request("POST","/eth/contracts/" + request['contract_address'] + "/read",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data']
+		})
 
 	def getContractWrite(self, request = {}):
 		if not('private_key' in request) or not request['private_key']:
@@ -262,16 +262,16 @@ class Ethereum(Base):
 
 		
 		return self.request("POST","/eth/contracts/" + request['contract_address'] + "/write",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "private_key" : request['private_key'],
-            "password" : request['password'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "private_key" : request['private_key'],
+		    "password" : request['password'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
         
 	def getContractWriteFees(self, request = {}):
 		if not('gas_limit' in request) or not request['gas_limit']:
@@ -284,11 +284,11 @@ class Ethereum(Base):
 
 		
 		return self.request("POST","/eth/contracts/" + request['contract_address'] + "/write/fees",{
-            "method" : request['method'],
-            "return_type" : request['return_type'],
-            "parameter_type" : request['parameter_type'],
-            "parameter_data" : request['parameter_data'],
-            "from" : request['from'],
-            "amount" : request['amount'],
-            "gas_limit" : request['gas_limit']
-        })
+		    "method" : request['method'],
+		    "return_type" : request['return_type'],
+		    "parameter_type" : request['parameter_type'],
+		    "parameter_data" : request['parameter_data'],
+		    "from" : request['from'],
+		    "amount" : request['amount'],
+		    "gas_limit" : request['gas_limit']
+		})
