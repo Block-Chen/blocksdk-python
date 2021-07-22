ab90df87a3b275b3cb8d3a0e99520fd7e2506d8b
blockchen-admin <61440698+blockchen-admin@users.noreply.github.com>
2021-07-22 17:59:01 +0900

    Update solc.py

--- BlockSDK/solc.py
+++ BlockSDK/solc.py
@@ -3,5 +3,5 @@ class Solc(Base):
 	def genContract(self, request = {}):
 		return self.request("GET","/solc/hash-type/",request)
     
-    def encodefunction(self, request = {}):
+    	def encodefunction(self, request = {}):
 		return self.request("POST","/solc/encodefunction/",request)
